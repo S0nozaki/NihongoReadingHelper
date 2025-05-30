d5babcaef23ab38087373a57651a760d34584489
Max Roger Morillo Alvarado <maxrogermorillo@gmail.com>
2025-05-30 02:28:51 -0300

    Add gui support with flet

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import easyocr
 import config
+import flet as ft
 
-reader = easyocr.Reader(['ja','en'])
-result = reader.readtext(config.IMG_PATH, detail = 0)
-print(result)
+def get_image_text():
+    reader = easyocr.Reader(['ja','en'])
+    result = reader.readtext(config.IMG_PATH, detail = 0)
+    return result
+
+
+def gui(page: ft.Page):
+    page.title = "Nihongo Reading Helper"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    tb = ft.TextField(label='Text extracted from image', read_only=True, value=get_image_text())
+    page.add(tb)
+
+ft.app(gui)
