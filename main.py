d109c9fa1d4a5021f21db1cc9a7e04702cadaab6
Max Roger Morillo Alvarado <maxrogermorillo@gmail.com>
2025-05-30 18:54:10 -0300

    Add processing image notice to lessen perceived wait until extraction completes

--- main.py
+++ main.py
@@ -4,18 +4,20 @@ import flet as ft
 from pynput.mouse import Listener
 from PIL import ImageGrab
 
-def get_image_text(img):
-    reader = easyocr.Reader(['ja','en'])
-    result = reader.readtext(img, detail=0)
+def get_image_text(img_path):
+    reader = easyocr.Reader(['ja','en'], gpu=False)
+    result = reader.readtext(img_path, detail=0)
     return result
 
 def gui(page: ft.Page):
     page.title = "Nihongo Reading Helper"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     
-    def get_selected_area(img):
-        tb = ft.TextField(label='Area', read_only=True, value=get_image_text(img))
+    def get_selected_area_text(img_path):
+        tb = ft.TextField(label='Area', read_only=True, value="Extracting text from selected area")
         page.add(tb)
+        tb.value = get_image_text(img_path)
+        page.update()
     def get_area(e):
         page.window.opacity = 0.01
         #for some reason 0.0 opacity not only makes it invisible, but treates it as non-existant
@@ -34,13 +36,13 @@ def gui(page: ft.Page):
                     area_final[1] = y
                     print("Final coordinates: ", (x,y))
             if not pressed:
+                listener.stop()
                 page.window.full_screen = False
                 page.window.opacity = 1.0
                 page.update()
                 area = ImageGrab.grab(bbox=(area_start[0], area_start[1], area_final[0], area_final[1]))
                 area.save(config.SAVE_TO_PATH, "JPEG")
-                listener.stop()
-                get_selected_area(config.SAVE_TO_PATH)
+                get_selected_area_text(config.SAVE_TO_PATH)
                 return True
         with Listener(on_click=on_click) as listener:
             listener.join()
