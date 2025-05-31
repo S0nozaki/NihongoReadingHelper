8445107c94a6cdbe4ed1f0e3b5ced410292412e8
Max Roger Morillo Alvarado <maxrogermorillo@gmail.com>
2025-05-31 01:49:11 -0300

    Add kanji extraction from selected text and displaying in grid

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ import config
 import flet as ft
 from pynput.mouse import Listener
 from PIL import ImageGrab
+import json
+
+def load_json(filename):
+    with open(filename, encoding="utf-8") as f:
+        d = json.load(f)
+    return d
+
+kanji2element = load_json("kanji2element.json")
+kanji2radical = load_json("kanji2radical.json")
+element2kanji = load_json("element2kanji.json")
+radical2kanji = load_json("radical2kanji.json")
 
 def get_image_text(img_path):
     reader = easyocr.Reader(['ja','en'], gpu=False)
@@ -39,11 +50,9 @@ def gui(page: ft.Page):
                 if pressed:
                     area_start[0] = x
                     area_start[1] = y
-                    print("Initial coordinates: ", (x,y))
                 else:
                     area_final[0] = x
                     area_final[1] = y
-                    print("Final coordinates: ", (x,y))
             if not pressed:
                 listener.stop()
                 page.window.full_screen = False
@@ -56,6 +65,43 @@ def gui(page: ft.Page):
         with Listener(on_click=on_click) as listener:
             listener.join()
     button = ft.Button("Select area to extract text", on_click=get_area)
-    page.add(button)
+
+    def get_kanji(e):
+        text_field_to_inspect = ""
+        text_fields = [field for field in page.controls if ('TextField' in str(type(field)))]
+        for field in text_fields:
+            text_field_to_inspect += field.value
+        
+        if text_field_to_inspect == "":
+            return False
+
+        kanji_to_show = extract_all_kanji(text_field_to_inspect)
+        
+        kanji_cards = []
+        for kanji in kanji_to_show:
+            kanji_cards.append(ft.Container(
+                content=ft.Text(kanji),
+                alignment=ft.alignment.center,
+                bgcolor=ft.Colors.AMBER,
+                width=150,
+                height=150,
+                margin=10,
+                padding=10,
+                border_radius=10,
+            ))
+        
+        page.add(ft.GridView(expand=1, runs_count=8, spacing=5, controls=kanji_cards))
+        page.update()
+
+    kanji_button = ft.Button("Extract kanji from selected text", on_click=get_kanji)
+    page.add(button, kanji_button)
+
+
+def extract_all_kanji(word):
+    kanji_list = []
+    for letter in word:
+        if letter in kanji2element or letter in radical2kanji:
+            kanji_list.append(letter)
+    return kanji_list
 
 ft.app(gui)
