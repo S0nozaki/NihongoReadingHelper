efebed803b755493572a5f8551ddc972c9dfe808
Max Roger Morillo Alvarado <maxrogermorillo@gmail.com>
2025-07-05 23:49:28 -0300

    Remove extra field per line in multiple line text image extraction

--- main.py
+++ main.py
@@ -27,17 +27,11 @@ def gui(page: ft.Page):
     def get_selected_area_text(img_path):
         extracted_text = ft.TextField(label='Extracted text', read_only=True, value="Extracting text from selected area")
         page.add(extracted_text)
-        img_text = get_image_text(img_path)
-        if len(img_text) < 2:
-            extracted_text.value = img_text[0]
-        else:
-            page.remove(extracted_text)
-            for word_group in img_text:
-                group_text = ft.TextField(label='Extracted text', read_only=True, value=word_group)
-                page.add(group_text)
         page.update()
-        
+        img_text = ''.join(get_image_text(img_path))
+        extracted_text.value = img_text
         page.update()
+
     def get_area(e):
         page.window.opacity = 0.01
         #for some reason 0.0 opacity not only makes it invisible, but treates it as non-existant
